ba614ed9241990e6124b43c6d0078de9338055e0
admin <Xiaoji52@>
2025-03-28 22:43:38 +1300

    yml

--- backend/note/models.py
+++ backend/note/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    user = models.ForeignKey('user', on_delete=models.CASCADE)
     def __str__(self):
         return self.title
 
