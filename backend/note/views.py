408f0be5a05d3856c5590efe46252b5faa94f259
admin <Xiaoji52@>
2025-03-30 21:25:26 +1300

    added Delete note feature

--- backend/note/views.py
+++ backend/note/views.py
@@ -1,14 +1,15 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from.models import Note
+
+from .models import Note
 
 class NoteViewManual(APIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication]
-
 
     def post(self, request):
         user = request.user
@@ -22,9 +23,14 @@ class NoteViewManual(APIView):
         return Response({"message": "Note created successfully", "note_id": note.id}, status=status.HTTP_201_CREATED)
 
     def put(self, request):
-        note = Note.objects.get(id=request.data.get('note_id'))
-        note.title=request.data.get('title', note.title)
-        note.content=request.data.get('content', note.content)
+        note = Note.objects.get(id=request.data.get("note_id"))
+        note.title = request.data.get('title', note.title)
+        note.content = request.data.get('content', note.content)
         note.save()
 
         return Response({"message": "Note updated successfully"}, status=status.HTTP_200_OK)
+
+    def delete(self, request):
+        note = get_object_or_404(Note, id=request.data.get("note_id"))
+        note.delete()
+        return Response({"message": "Note deleted successfully"}, status=status.HTTP_200_OK)
